63d4983386e0e85e80b7ad2db7ee1b6104042c2a
namantewari10 <namantewari10@gmail.com>
2021-05-06 03:43:48 +0530

    Added two random comments

--- app/src/main/java/com/tewari/githubdemorepo/MainActivity.java
+++ app/src/main/java/com/tewari/githubdemorepo/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //This is unknown branch and I am just playing around
+        //Added 2nd comment
     }
 }
